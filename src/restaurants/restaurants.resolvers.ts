c51959a9d63864e1498646fa3bbd604e1dcf9252
gilpop8663 <wolfye@naver.com>
2024-01-06 04:56:34 +0900

    feat: Update Restaurant

--- src/restaurants/restaurants.resolvers.ts
+++ src/restaurants/restaurants.resolvers.ts
@@ -2,6 +2,7 @@ import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { Restaurant } from './entites/restaurant.entity';
 import { CreateRestaurantDto } from './dtos/create-restaurant.dto';
 import { RestaurantsService } from './restaurants.service';
+import { UpdateRestaurantDto } from './dtos/update.restaurant.dto';
 
 @Resolver(() => Restaurant)
 export class RestaurantResolver {
@@ -17,6 +18,15 @@ export class RestaurantResolver {
   @Mutation(() => Boolean)
   async createRestaurant(@Args('input') restaurantData: CreateRestaurantDto) {
     await this.restaurantService.createRestaurant(restaurantData);
+
+    return true;
+  }
+
+  @Mutation(() => Boolean)
+  async updateRestaurant(
+    @Args('input') updateRestaurantDto: UpdateRestaurantDto,
+  ) {
+    await this.restaurantService.updateRestaurant(updateRestaurantDto);
 
     return true;
   }
--- src/restaurants/restaurants.service.ts
+++ src/restaurants/restaurants.service.ts
@@ -3,6 +3,7 @@ import { Restaurant } from './entites/restaurant.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateRestaurantDto } from './dtos/create-restaurant.dto';
+import { UpdateRestaurantDto } from './dtos/update.restaurant.dto';
 
 @Injectable()
 export class RestaurantsService {
@@ -19,5 +20,9 @@ export class RestaurantsService {
     const newRestaurant = this.restaurantsRepository.create(restaurantData);
 
     return this.restaurantsRepository.save(newRestaurant);
+  }
+
+  updateRestaurant({ id, data }: UpdateRestaurantDto) {
+    return this.restaurantsRepository.update(id, data);
   }
 }
